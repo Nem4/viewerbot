0417fc2ad86824f18b51dfcee92a4bbe276010e2
Vladyslav Niemchencko <vniemchenko@gmail.com>
2019-02-16 15:48:58 -0500

    small changes

--- getproxy.py
+++ getproxy.py
@@ -3,66 +3,111 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import random
 
+
 class GetProxy():
 
-  def __init__(self):
-    self.ua = UserAgent() # From here we generate a random user agent
-    self.proxies = [] # Will contain self.proxies [ip, port]
-    import os
-    if os.path.exists("proxy.txt"):
-      os.remove("proxy.txt")
+    def __init__(self, delete_proxy = False):
+        self.ua = UserAgent()  # From here we generate a random user agent
+        self.proxies = []  # Will contain self.proxies [ip, port]
+        if delete_proxy:
+            import os
+            if os.path.exists("proxy_list.txt"):
+                os.remove("proxy_list.txt")
 
-  # Main function
-  def get_proxy(self):
-    # Retrieve latest self.proxies
-    proxies_req = Request('https://www.sslproxies.org/')
-    proxies_req.add_header('User-Agent', self.ua.random)
-    proxies_doc = urlopen(proxies_req).read().decode('utf8')
+    # Main function
+    def get_proxy(self):
+        # Retrieve latest self.proxies
+        proxies_req = Request('https://www.sslproxies.org/')
+        proxies_req.add_header('User-Agent', self.ua.random)
+        proxies_doc = urlopen(proxies_req).read().decode('utf8')
 
-    soup = BeautifulSoup(proxies_doc, 'html.parser')
-    proxies_table = soup.find(id='proxylisttable')
+        soup = BeautifulSoup(proxies_doc, 'html.parser')
+        proxies_table = soup.find(id='proxylisttable')
 
-    # Save self.proxies in the array
-    for row in proxies_table.tbody.find_all('tr'):
-      self.proxies.append({
-        'ip':   row.find_all('td')[0].string,
-        'port': row.find_all('td')[1].string
-      })
+        # Save self.proxies in the array
+        for row in proxies_table.tbody.find_all('tr'):
+            self.proxies.append({
+                'ip': row.find_all('td')[0].string,
+                'port': row.find_all('td')[1].string
+            })
 
-    # Choose a random proxy
-    proxy_index = self.random_proxy()
-    proxy = self.proxies[proxy_index]
-    already_saved = False
-    for n in range(1, 100):
-      req = Request('http://icanhazip.com')
-      req.set_proxy(proxy['ip'] + ':' + proxy['port'], 'http')
+        proxies_req = Request('http://spys.me/proxy.txt')
+        proxies_req.add_header('User-Agent', self.ua.random)
+        proxies_doc = urlopen(proxies_req).read().decode('utf8')
 
-      # Every 10 requests, generate a new proxy
-      if n % 6 == 0:
-        proxy_index = self.random_proxy()
-        proxy = self.proxies[proxy_index]
-        already_saved = False
+        soup = BeautifulSoup(proxies_doc, 'html.parser').text
+        # proxies_table = soup.find('pre')
+        # print("******START******")
+        all_proxies = soup[216: -69]
+        all_lines = all_proxies.splitlines()
+        for line in all_lines:
+            host = line[: line.find(':')]
+            port = line[line.find(':') + 1:line.find(' ')]
+            self.proxies.append({
+                'ip': host,
+                'port': port
+            })
 
-      # Make the call
-      try:
-        my_ip = urlopen(req).read().decode('utf8')
-        print('#' + str(n) + ': ' + my_ip)
-        if n % 5 == 0 and already_saved == False:
-          self.save_proxy(proxy['ip'], proxy['port'])
-          already_saved = True
-      except: # If error, delete this proxy and find another one
-        del self.proxies[proxy_index]
-        print('Proxy ' + proxy['ip'] + ':' + proxy['port'] + ' deleted.')
-        proxy_index = self.random_proxy()
-        proxy = self.proxies[proxy_index]
+        proxies_req = Request('https://proxy.rudnkh.me/txt')
+        proxies_req.add_header('User-Agent', self.ua.random)
+        proxies_doc = urlopen(proxies_req).read().decode('utf8')
 
-  # Retrieve a random index proxy (we need the index to delete it if not working)
-  def random_proxy(self):
-    return random.randint(0, len(self.proxies) - 1)
+        soup = BeautifulSoup(proxies_doc, 'html.parser').text
+        # proxies_table = soup.find('pre')
+        # print("******START******")
+        all_proxies = soup
+        all_lines = all_proxies.splitlines()
+        for line in all_lines:
+            # print(line[len(line)-2: ].strip())
+            if line[len(line) - 2:].strip() == "+":
+                host = line[: line.find(':')]
+                port = line[line.find(':') + 1:line.find(' ')]
+                self.proxies.append({
+                    'ip': host,
+                    'port': port
+                })
 
-  def save_proxy(self, proxy_host, proxy_port):
-    proxys = open("proxy.txt", "a+")
-    proxys.write(f"{proxy_host}:{proxy_port}" + '\n')
+                # print(f"host #: {line[ : line.find(':')]}")
+                # print(f"port #: {line[line.find(':') + 1:line.find(' ') ]}")
+        # print(all_proxies)
+        # print("******END******")
+        for p in self.proxies:
+            self.save_proxy(p['ip'], p['port'])
+        # Choose a random proxy
+        # proxy_index = self.random_proxy()
+        # proxy = self.proxies[proxy_index]
+        # already_saved = False
+        # for n in range(1, 100):
+        #     req = Request('http://icanhazip.com')
+        #     req.set_proxy(proxy['ip'] + ':' + proxy['port'], 'http')
+        #
+        #     # Every 10 requests, generate a new proxy
+        #     if n % 3 == 0:
+        #         proxy_index = self.random_proxy()
+        #         proxy = self.proxies[proxy_index]
+        #         already_saved = False
+        #
+        #     # Make the call
+        #     try:
+        #         my_ip = urlopen(req).read().decode('utf8')
+        #         print('#' + str(n) + ': ' + my_ip)
+        #         if n % 2 == 0 and already_saved == False:
+        #             self.save_proxy(proxy['ip'], proxy['port'])
+        #             already_saved = True
+        #     except:  # If error, delete this proxy and find another one
+        #         del self.proxies[proxy_index]
+        #         print('Proxy ' + proxy['ip'] + ':' + proxy['port'] + ' deleted.')
+        #         proxy_index = self.random_proxy()
+        #         proxy = self.proxies[proxy_index]
 
-# gp = GetProxy()
-# gp.get_proxy()
+    # Retrieve a random index proxy (we need the index to delete it if not working)
+    def random_proxy(self):
+        return random.randint(0, len(self.proxies) - 1)
+
+    def save_proxy(self, proxy_host, proxy_port):
+        proxys = open("proxy_list.txt", "a+")
+        proxys.write(f"{proxy_host}:{proxy_port}" + '\n')
+
+
+gp = GetProxy()
+gp.get_proxy()
---
+++ viewer.py
@@ -0,0 +1,129 @@
+'''
+Copyright 2015 ohyou
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+import requests
+import subprocess
+import json
+import sys
+import multiprocessing
+import time
+import random
+
+channel_url = "twitch.tv/al_twitch_123"
+processes = []
+
+
+def get_channel():
+    # Reading the channel name - passed as an argument to this script
+    if len(sys.argv) >= 2:
+        global channel_url
+        channel_url += sys.argv[1]
+    else:
+        print("An error has occurred while trying to read arguments. Did you specify the channel?")
+        sys.exit(1)
+
+
+def get_proxies():
+    # Reading the list of proxies
+    try:
+        lines = [line.rstrip("\n") for line in open("proxy.txt")]
+    except IOError as e:
+        print("An error has occurred while trying to read the list of proxies: %s" % e.strerror)
+        sys.exit(1)
+
+    return lines
+
+
+def get_url():
+    # Getting the json with all data regarding the stream
+    try:
+        response = subprocess.Popen(
+            ["livestreamer.exe", "--http-header", "Client-ID=ewvlchtxgqq88ru9gmfp1gmyt6h2b93",
+            channel_url, "-j"], stdout=subprocess.PIPE).communicate()[0]
+    except subprocess.CalledProcessError:
+        print("An error has occurred while trying to get the stream data. Is the channel online? Is the channel name correct?")
+        sys.exit(1)
+    except OSError:
+        print("An error has occurred while trying to use livestreamer package. Is it installed? Do you have Python in your PATH variable?")
+
+    # Decoding the url to the worst quality of the stream
+    try:
+        url = json.loads(response)['streams']['audio_only']['url']
+    except:
+        try:
+            url = json.loads(response)['streams']['worst']['url']
+        except (ValueError, KeyError):
+            print("An error has occurred while trying to get the stream data. Is the channel online? Is the channel name correct?")
+            sys.exit(1)
+
+    return url
+
+
+def open_url(url, proxy):
+    # Sending HEAD requests
+    while True:
+        try:
+            with requests.Session() as s:
+                response = s.head(url, proxies=proxy)
+            print("Sent HEAD request with %s" % proxy["http"])
+            time.sleep(20)
+        except requests.exceptions.Timeout:
+            print("  Timeout error for %s" % proxy["http"])
+        except requests.exceptions.ConnectionError:
+            print("  Connection error for %s" % proxy["http"])
+
+
+def prepare_processes():
+    global processes
+    proxies = get_proxies()
+    n = 0
+
+    if len(proxies) < 1:
+        print("An error has occurred while preparing the process: Not enough proxy servers. Need at least 1 to function.")
+        sys.exit(1)
+
+    for proxy in proxies:
+        # Preparing the process and giving it its own proxy
+        processes.append(
+            multiprocessing.Process(
+                target=open_url, kwargs={
+                    "url": get_url(), "proxy": {
+                        "http": proxy}}))
+
+        print('.')
+    print(f"Max viewers: {len(proxies)}")
+    print('')
+
+if __name__ == "__main__":
+    print("Obtaining the channel...")
+    # get_channel()
+    print("Obtained the channel")
+    print("Preparing the processes...")
+    prepare_processes()
+    print("Prepared the processes")
+    print("Booting up the processes...")
+
+    # Timer multiplier
+    n = 8
+
+    # Starting up the processes
+    for process in processes:
+        time.sleep(random.randint(1, 5) * n)
+        process.daemon = True
+        process.start()
+        if n > 1:
+            n -= 1
+
+    # Running infinitely
+    while True:
+        time.sleep(1)
