e937ba95dc72e025067bd0f8c431229042fd8954
Vladyslav Niemchenko <vniemchenko@gmail.com>
2019-02-26 20:25:20 -0500

    small changes

--- viewer.py
+++ viewer.py
@@ -18,14 +18,23 @@ import sys
 import multiprocessing
 import time
 import random
+from random import shuffle
+from fake_useragent import UserAgent
 
 channel_url = "twitch.tv/agantor"
+# channel_url = "https://www.twitch.tv/mgberg"
+channel_url = "twitch.tv/piggyesgames"
+channel_url = "https://www.twitch.tv/spirit_stan"
+channel_url = "https://www.twitch.tv/comboedow"
 proxies_file = "Proxies_txt/good_proxy.txt"
 processes = []
-
-from Proxy.find_and_save import ProxyFinder
-pf = ProxyFinder(2000)
-sys.exit(0)
+max_viewers = 1000
+# all_processes = []
+nb_of_proxies = 0
+ua = UserAgent()
+# from Proxy.find_and_save import ProxyFinder
+# pf = ProxyFinder(2000)
+# sys.exit(0)
 
 def get_channel():
     # Reading the channel name - passed as an argument to this script
@@ -39,12 +48,14 @@ def get_channel():
 
 def get_proxies():
     # Reading the list of proxies
+    global nb_of_proxies
     try:
         lines = [line.rstrip("\n") for line in open(proxies_file)]
     except IOError as e:
         print("An error has occurred while trying to read the list of proxies: %s" % e.strerror)
         sys.exit(1)
 
+    nb_of_proxies = len(lines)
     return lines
 
 
@@ -73,48 +84,88 @@ def get_url():
     return url
 
 
-def open_url( proxy):
+def open_url(proxy, all_proxies):
     # Sending HEAD requests
+    shuffle(all_proxies)
     nb_of_tries = 0
+    nb_of_proxies_failures = 0
     url = get_url()
+    max_proxy_failure = 20
+    # url = get_url()
     # time.sleep(random.randint(4, 15))
+    current_proxy = proxy
     while True:
+        headers = {
+            'User-Agent': ua.random
+        }
         try:
             with requests.Session() as s:
-                response = s.head(url, proxies=proxy)
-            print(f"Sent HEAD request with {proxy['http']} | {response.content} | {response.text} | {response}")
+                response = s.head(url, proxies=current_proxy, headers=headers)
+            print(f"Sent HEAD request with {current_proxy['http']} | {response.status_code} | {response.request} | {response}")
+            if response.status_code == 503:
+                nb_of_tries += 1
+                # print("  SERVICE UNAVAILABLE %s" % proxy["http"])
+                if nb_of_tries > 1:
+                    if nb_of_proxies_failures > max_proxy_failure:
+                        print("################### Closing this thread! ################### ")
+                        break
+                    else:
+                        print("################### CHANGING PROXY ################### ")
+                        current_proxy = {"http": all_proxies[0]}
+                        nb_of_proxies_failures += 1
             time.sleep(20)
         except requests.exceptions.Timeout:
             nb_of_tries += 1
-            print("  Timeout error for %s" % proxy["http"])
-            if nb_of_tries > 3:
-                print("################### Closing this thread! ################### ")
-                break
+            print("  Timeout error for %s" % current_proxy["http"])
+            if nb_of_tries > 1:
+                if nb_of_proxies_failures > max_proxy_failure:
+                    print("################### Closing this thread! ################### ")
+                    break
+                else:
+                    print("################### CHANGING PROXY ################### ")
+                    current_proxy = {"http": all_proxies[0]}
+                    nb_of_proxies_failures += 1
         except requests.exceptions.ConnectionError:
             nb_of_tries += 1
-            print("  Connection error for %s" % proxy["http"])
-            if nb_of_tries > 3:
-                print("################### Closing this thread! ################### ")
-                break
+            print("  Connection error for %s" % current_proxy["http"])
+            if nb_of_tries > 1:
+                if nb_of_proxies_failures > max_proxy_failure:
+                    print("################### Closing this thread! ################### ")
+                    break
+                else:
+                    print("################### CHANGING PROXY ################### ")
+                    current_proxy = {"http": all_proxies[0]}
+                    nb_of_proxies_failures += 1
 
 
 def prepare_processes():
     global processes
     proxies = get_proxies()
-
+    shuffle(proxies)
     if len(proxies) < 1:
         print("An error has occurred while preparing the process: Not enough proxy servers. Need at least 1 to function.")
         sys.exit(1)
 
+    n = 0
     for proxy in proxies:
-        # Preparing the process and giving it its own proxy
-        processes.append(
-            multiprocessing.Process(
-                target=open_url, kwargs={
-                    "proxy": {
-                        "http": proxy}}))
+        if n<max_viewers:
+            # Preparing the process and giving it its own proxy
+            # processes.append(
+            #     multiprocessing.Process(
+            #         target=open_url, kwargs={"all_proxies": proxies,
+            #             "proxy": {
+            #                 "http": proxy}}))
+            p = multiprocessing.Process(
+                    target=open_url, kwargs={"all_proxies": proxies,
+                                             "proxy": {
+                                                 "http": proxy}})
+            p.daemon = True
+            p.start()
 
-        print('.')
+            n += 1
+            print(f'Prepearing # {n} of {len(proxies)}')
+        else:
+            break
     print(f"Max viewers: {len(proxies)}")
     print('')
 
@@ -128,16 +179,22 @@ if __name__ == "__main__":
     print("Booting up the processes...")
 
     # Timer multiplier
-    n = 8
+    # n = 8
 
     # Starting up the processes
-    for process in processes:
-        time.sleep(random.randint(1, 2) * n)
-        process.daemon = True
-        process.start()
-        if n > 1:
-            n -= 1
+    # for process in processes:
+    #     time.sleep(random.randint(1, 2))
+    #     process.daemon = True
+    #     process.start()
+    #     if n > 1:
+    #         n -= 1
 
     # Running infinitely
     while True:
+        # nb = 0
+        # # time.sleep(random.randint(1, 2))
+        # for p in processes:
+        #     if p.is_alive():
+        #         nb+=1
+        # print(f"Running {nb} of {max_viewers}")
         time.sleep(1)
