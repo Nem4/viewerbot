8651eab366d688e9f9ab59b7d422c1d2d5963c86
LordCreations <33207253+iLoooordFx@users.noreply.github.com>
2021-07-29 04:57:55 -0300

    Working again
    Livestream removed, using Streamlink

--- threads_viewer.py
+++ threads_viewer.py
@@ -1,5 +1,5 @@
 import requests
-from livestreamer import Livestreamer
+from streamlink import Streamlink
 import sys
 import time
 import random
@@ -10,7 +10,7 @@ import linecache
 from threading import Thread
 
 channel_url = ""
-proxies_file = "Proxies_txt/good_proxy.txt"
+proxies_file = "good_proxy.txt"
 processes = []
 max_nb_of_threads = 1000
 
@@ -19,102 +19,95 @@ nb_of_proxies = 0
 
 # Session creating for request
 ua = UserAgent()
-session = Livestreamer()
+session = Streamlink()
 session.set_option("http-headers", {'User-Agent': ua.random, "Client-ID": "ewvlchtxgqq88ru9gmfp1gmyt6h2b93"})
 
-
-def print_exception():
-    exc_type, exc_obj, tb = sys.exc_info()
-    f = tb.tb_frame
-    lineno = tb.tb_lineno
-    filename = f.f_code.co_filename
-    linecache.checkcache(filename)
-    line = linecache.getline(filename, lineno, f.f_globals)
-    print('EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj))
-
-
-def get_channel():
-    # Reading the channel name - passed as an argument to this script
-    if len(sys.argv) >= 2:
-        global channel_url
-        channel_url += sys.argv[1]
-    else:
-        print("An error has occurred while trying to read arguments. Did you specify the channel?")
-        sys.exit(1)
+class ViewerBot:
+    def print_exception(self):
+        exc_type, exc_obj, tb = sys.exc_info()
+        f = tb.tb_frame
+        lineno = tb.tb_lineno
+        filename = f.f_code.co_filename
+        linecache.checkcache(filename)
+        line = linecache.getline(filename, lineno, f.f_globals)
+        print('EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj))
 
 
-def get_proxies():
-    # Reading the list of proxies
-    global nb_of_proxies
-    try:
-        lines = [line.rstrip("\n") for line in open(proxies_file)]
-    except IOError as e:
-        print("An error has occurred while trying to read the list of proxies: %s" % e.strerror)
-        sys.exit(1)
 
-    nb_of_proxies = len(lines)
-    return lines
-
-
-def get_url():
-    url = ""
-    try:
-        streams = session.streams(channel_url)
+    def get_proxies(self):
+        # Reading the list of proxies
+        global nb_of_proxies
         try:
-            url = streams['audio_only'].url
-        except:
-            url = streams['worst'].url
-    except:
-        print("Wasnt able to get url")
-    return url
+            lines = [line.rstrip("\n") for line in open(proxies_file)]
+        except IOError as e:
+            print("An error has occurred while trying to read the list of proxies: %s" % e.strerror)
+            sys.exit(1)
 
-def open_url(proxy_data):
-    try:
-        global all_proxies
-        headers = {'User-Agent': ua.random}
-        current_index = all_proxies.index(proxy_data)
+        nb_of_proxies = len(lines)
+        return lines
 
-        if proxy_data['url'] == "":
-            proxy_data['url'] = get_url()
-        current_url = proxy_data['url']
+
+    def get_url(self):
+        url = ""
         try:
-             if time.time() - proxy_data['time'] >= random.randint(1, 5):
-                current_proxy = {"http": proxy_data['proxy'], "https": proxy_data['proxy']}
-                with requests.Session() as s:
-                    response = s.head(current_url, proxies=current_proxy, headers=headers)
-                print(f"Sent HEAD request with {current_proxy['http']} | {response.status_code} | {response.request} | {response}")
-                proxy_data['time'] = time.time()
-                all_proxies[current_index] = proxy_data
+            streams = session.streams(self.channel_url)
+            try:
+                url = streams['audio_only'].url
+                print(f"URL : {url[:30]}...\n")
+            except:
+                url = streams['worst'].url
+                print(f"URL : {url[:30]}...\n")
+
         except:
-            print("************* Connection Error! *************")
+            pass
+        return url
 
-    except (KeyboardInterrupt, SystemExit):
-        sys.exit()
-
-
-if __name__ == "__main__":
-
-    if len(sys.argv) == 1:
-        channel_url = "https://www.twitch.tv/" + sys.argv[1]
-    else:
-        channel_url = "https://www.twitch.tv/your_channel_name"
-    start_time = time.time()
-    proxies = get_proxies()
-    
-    for p in proxies:
-        all_proxies.append({'proxy': p, 'time': time.time(), 'url': ""})
-
-    shuffle(all_proxies)
-    list_of_all_proxies = all_proxies
-    current_proxy_index = 0
-
-    while True:
+    def open_url(self,proxy_data):
         try:
-            for i in range(0, max_nb_of_threads):
-                threaded = Thread(target=open_url, args=(all_proxies[random.randint(0, len(all_proxies))],))
-                threaded.daemon = True  # This thread dies when main thread (only non-daemon thread) exits.
-                threaded.start()
-        except:
-            print_exception()
+            global all_proxies
+            headers = {'User-Agent': ua.random}
+            current_index = all_proxies.index(proxy_data)
+
+            if proxy_data['url'] == "":
+                proxy_data['url'] = self.get_url()
+            current_url = proxy_data['url']
+            try:
+                 if time.time() - proxy_data['time'] >= random.randint(1, 5):
+                    current_proxy = {"http": proxy_data['proxy'], "https": proxy_data['proxy']}
+                    with requests.Session() as s:
+                        response = s.head(current_url, proxies=current_proxy, headers=headers)
+                    print(f"Sent HEAD request with {current_proxy['http']} | {response.status_code} | {response.request} | {response}")
+                    proxy_data['time'] = time.time()
+                    all_proxies[current_index] = proxy_data
+            except:
+                print("Connection Error!")
+
+        except (KeyboardInterrupt, SystemExit):
+            sys.exit()
+
+
+    def mainmain(self):
+        self.channel_url = "https://www.twitch.tv/" + sys.argv[1]
+        proxies = self.get_proxies()
+        
+        for p in proxies:
+            all_proxies.append({'proxy': p, 'time': time.time(), 'url': ""})
+
         shuffle(all_proxies)
-        time.sleep(5)
+        list_of_all_proxies = all_proxies
+        current_proxy_index = 0
+
+        while True:
+            try:
+                for i in range(0, max_nb_of_threads):
+                    threaded = Thread(target=self.open_url, args=(all_proxies[random.randint(0, len(all_proxies))],))
+                    threaded.daemon = True  # This thread dies when main thread (only non-daemon thread) exits.
+                    threaded.start()
+            except:
+                self.print_exception()
+            shuffle(all_proxies)
+            time.sleep(5)
+
+
+ViewerBot = ViewerBot()
+ViewerBot.mainmain()
